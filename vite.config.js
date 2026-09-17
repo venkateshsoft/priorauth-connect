309056baf2c456f05acbcd29707e4757b6887c84
Venkatesh Krishnan <venkatesh.soft@gmail.com>
2026-09-17 13:16:41 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,4 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-const repository = globalThis.process?.env?.GITHUB_REPOSITORY?.split('/')[1];
-export default defineConfig({ base: repository ? `/${repository}/` : '/', plugins: [react()] });
